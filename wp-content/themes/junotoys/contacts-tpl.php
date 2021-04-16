09c170219a70b91938c0b7ddf37b35fddef765eb
Dmytro Bordiuh <dmytro.bordiuh@nure.ua>
2021-04-16 21:29:55 +0300

    added fondy added contacts-tpl

---
+++ wp-content/themes/junotoys/contacts-tpl.php
@@ -0,0 +1,155 @@
+<?php
+/*
+ * Template Name: Контакти
+ * */
+
+get_header();
+while (have_posts()){
+    the_post();
+}
+?>
+    <div class="page_content_wrap page_paddings_no">
+        <div class="content_wrap">
+            <div class="content">
+                <article class="post_item post_item_single post-15 page hentry">
+                    <section class="post_content">
+                        <div class="vc_row wpb_row vc_row-fluid">
+                            <div class="wpb_column vc_column_container vc_col-sm-12">
+                                <div class="vc_column-inner ">
+                                    <div class="wpb_wrapper">
+                                        <div class="vc_empty_space em_height_6-5">
+                                            <span class="vc_empty_space_inner"></span>
+                                        </div>
+                                        <iframe class="sc_googlemap"
+                                                src="https://maps.google.com/maps?t=m&output=embed&iwloc=near&z=10&q=7337+Trade+St%2C+San+Diego%2C+CA+92121"
+                                                aria-label="7337 Trade St, San Diego, CA 92121"></iframe>
+                                        <div class="vc_empty_space em_height_6-5">
+                                            <span class="vc_empty_space_inner"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="vc_row wpb_row vc_row-fluid">
+                            <div class="wpb_column vc_column_container vc_col-sm-12">
+                                <div class="vc_column-inner ">
+                                    <div class="wpb_wrapper">
+                                        <div class="columns_wrap sc_columns columns_nofluid sc_columns_count_4">
+                                            <div class="column-1_4 sc_column_item">
+                                                <div class="sc_section sc_section_block aligncenter">
+                                                    <div class="sc_section_inner">
+                                                        <div class="sc_section_content_wrap">
+                                                            <figure class="sc_image sc_image_shape_square">
+                                                                <img src="images/1-1.png" alt="" />
+                                                            </figure>
+                                                            <h5 class="sc_title sc_title_regular sc_align_center text_uppercase">Адреса</h5>
+                                                            <div class="wpb_text_column wpb_content_element ">
+                                                                <div class="wpb_wrapper">
+                                                                    <p>проспект Науки, 14, Харків, Харківська область, 61000</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="column-1_4 sc_column_item">
+                                                <div class="sc_section sc_section_block aligncenter">
+                                                    <div class="sc_section_inner">
+                                                        <div class="sc_section_content_wrap">
+                                                            <figure class="sc_image sc_image_shape_square">
+                                                                <img src="images/2-1.png" alt="" />
+                                                            </figure>
+                                                            <h5 class="sc_title sc_title_regular sc_align_center text_uppercase">Мобільний телефон</h5>
+                                                            <div class="wpb_text_column wpb_content_element ">
+                                                                <div>(096) 482 21 50</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="column-1_4 sc_column_item">
+                                                <div class="sc_section sc_section_block aligncenter">
+                                                    <div class="sc_section_inner">
+                                                        <div class="sc_section_content_wrap">
+                                                            <figure class="sc_image sc_image_shape_square">
+                                                                <img src="images/3-1.png" alt="" />
+                                                            </figure>
+                                                            <h5 class="sc_title sc_title_regular sc_align_center text_uppercase">Наша скринька</h5>
+                                                            <div class="wpb_text_column wpb_content_element ">
+                                                                <div>juno@gmail.com</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="column-1_4 sc_column_item">
+                                                <div class="sc_section sc_section_block aligncenter">
+                                                    <div class="sc_section_inner">
+                                                        <div class="sc_section_content_wrap">
+                                                            <figure class="sc_image sc_image_shape_square">
+                                                                <img src="images/4-1a.png" alt="" />
+                                                            </figure>
+                                                            <h5 class="sc_title sc_title_regular sc_align_center text_uppercase">Наша Веб-сторінка</h5>
+                                                            <div class="wpb_text_column wpb_content_element ">
+                                                                <div>www.juno.com</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="vc_empty_space em_height_4-2">
+                                            <span class="vc_empty_space_inner"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div data-vc-full-width="true" data-vc-full-width-init="false" class="vc_row wpb_row vc_row-fluid vc_custom_1464101475265">
+                            <div class="wpb_column vc_column_container vc_col-sm-12">
+                                <div class="vc_column-inner ">
+                                    <div class="wpb_wrapper">
+                                        <div class="vc_empty_space em_height_12">
+                                            <span class="vc_empty_space_inner"></span>
+                                        </div>
+                                        <h1 class="sc_title sc_title_regular sc_align_center text_uppercase">Обратний зв'язок</h1>
+                                        <div id="sc_form_777_wrap" class="sc_form_wrap">
+                                            <div id="sc_form_777" class="sc_form sc_form_style_form_1 aligncenter">
+                                                <form id="sc_form_777_form" class="sc_input_hover_default" data-formtype="form_1" method="post" action="includes/sendmail.php">
+                                                    <div class="sc_form_info">
+                                                        <div class="columns_wrap">
+                                                            <div class="sc_form_item sc_form_field label_over column-1_2">
+                                                                <input id="sc_form_username" type="text" name="username" placeholder="Name *" aria-required="true">
+                                                            </div>
+                                                            <div class="sc_form_item sc_form_field label_over column-1_2">
+                                                                <input id="sc_form_email" type="text" name="email" placeholder="E-mail *" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                        <div class="sc_form_item sc_form_field label_over">
+                                                            <input id="sc_form_subj" type="text" name="subject" placeholder="Subject" aria-required="true"> </div>
+                                                    </div>
+                                                    <div class="sc_form_item sc_form_message">
+                                                        <textarea id="sc_form_message" name="message" placeholder="Message" aria-required="true"></textarea>
+                                                    </div>
+                                                    <button class="sc_form_item sc_form_button sc_button sc_button_color_1 small sc_button_size_small">Відправити повідомлення</button>
+                                                    <div class="result sc_infobox"></div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="vc_empty_space em_height_6-7">
+                                            <span class="vc_empty_space_inner"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="vc_row-full-width"></div>
+                    </section>
+                </article>
+                <section class="related_wrap related_wrap_empty"></section>
+            </div>
+        </div>
+    </div>
+
+<?php
+get_footer("woocommerce");
---
+++ wp-content/themes/junotoys/footer-woocommerce.php
@@ -0,0 +1,49 @@
+<footer class="contacts_wrap scheme_original">
+    <div class="contacts_wrap_inner">
+        <div class="content_wrap">
+            <div class="logo">
+                <a href="index.html">
+                    <img src="images/logo-footer.png" class="logo_footer" alt="" width="121" height="60">
+                </a>
+            </div>
+            <div class="contact_info">Зробіть час гри найвишуканішими іграшками та іграми!</div>
+            <div class="sc_socials sc_socials_type_images sc_socials_shape_square sc_socials_size_large">
+                <div class="sc_socials_item">
+                    <a href="#" target="_blank" class="social_icons social_twitter">
+                        <span class="sc_socials_hover"></span>
+                    </a>
+                </div>
+                <div class="sc_socials_item">
+                    <a href="#" target="_blank" class="social_icons social_facebook">
+                        <span class="sc_socials_hover"></span>
+                    </a>
+                </div>
+                <div class="sc_socials_item">
+                    <a href="#" target="_blank" class="social_icons social_pinterest">
+                        <span class="sc_socials_hover"></span>
+                    </a>
+                </div>
+                <div class="sc_socials_item">
+                    <a href="#" target="_blank" class="social_icons social_linkedin">
+                        <span class="sc_socials_hover"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="copyright_wrap copyright_style_text  scheme_original">
+    <div class="copyright_wrap_inner">
+        <div class="content_wrap">
+            <div class="copyright_text">
+                <figure class="sc_image alignright sc_image_shape_square">
+                    <img src="images/Visa.png" alt="" />
+                </figure>
+                <p>Juno © 2021 Всі права захищені.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+wp_footer();
+
